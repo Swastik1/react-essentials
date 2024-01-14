042449da642144e506a2c0a0106189a8563d3f3c
Swastik <swastikpanda32@gmail.com>
2024-01-14 20:54:30 +0530

    added header component and outputting dynamic files & content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import reactImage from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+const genRandomInt = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+};
+
+const Header = () => {
+  const descriptions = reactDescriptions[genRandomInt(2)];
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {descriptions} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+};
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
