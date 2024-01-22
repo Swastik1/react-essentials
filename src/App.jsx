29340da3454ae83114b80e1f8569c51d4e32e18d
Swastik <swastikpanda32@gmail.com>
2024-01-22 08:39:01 +0530

    splliting components by feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcept/CoreConcepts";
-import Examples from "./components/CoreConcept/Examples";
+import Examples from "./components/TabButton/Examples";
 
 function App() {
   return (
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,10 +1,10 @@
 import "./TabButton.css";
 
-const TabButton = ({ children, onSelect, isSelected }) => {
+const TabButton = ({ children, isSelected, ...props }) => {
   console.log("Tab button component executed!");
   return (
     <li>
-      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+      <button className={isSelected ? "active" : undefined} {...props}>
         {children}
       </button>
     </li>
